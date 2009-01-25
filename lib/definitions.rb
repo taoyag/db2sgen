6da302737e7e82777d9afb0a707992dc1274ddec
taoyag <taoyag@a4b094fa-0a46-0410-99e9-af17c8c2d759>
2009-01-25 12:49:07 +0000

    git-svn-id: https://taoyag.org/repos/private/db2sgen@215 a4b094fa-0a46-0410-99e9-af17c8c2d759

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -4,16 +4,18 @@ require 'activerecord'
 class TableDefinition
   attr_accessor :table_name, :columns
 
-  def self.generate(table_name)
-    class_name = "A#{table_name}"
-    eval %-
-    class #{class_name} < ActiveRecord::Base
-      set_table_name '#{table_name}'
-    end
-    -
+  def self.collect(tables)
+    tables.map do |table_name|
+      class_name = "A#{table_name}"
+      eval %-
+      class #{class_name} < ActiveRecord::Base
+        set_table_name '#{table_name}'
+      end
+      -
 
-    table = const_get(class_name)
-    TableDefinition.new(table_name, table.columns)
+      table = const_get(class_name)
+      TableDefinition.new(table_name, table.columns)
+    end
   end
 
   def initialize(table_name, columns)
